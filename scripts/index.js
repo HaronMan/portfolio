bc32be25ce7bea03006009a633cfd99b6090fd51
HaronMan <h.kochbati59@gmail.com>
2024-03-20 16:52:09 +0100

    Page accueil terminé, plus qu'à ajouter l'image

--- scripts/index.js
+++ scripts/index.js
@@ -1,30 +1,48 @@
 const contenu = document.getElementById("contenu")
 const current_stylesheet = document.getElementById("current_stylesheet")
+const current_script = document.getElementById("current_script")
 
-function changerContenu(nav_elt) {
-    fetch("view/"+nav_elt.id+".html")
-    .then(response => {
-        if (!response.ok) {
-            throw new Error('Erreur de chargement du fichier HTML');
-        }
-        return response.text();
-    })
-    .then(html => {
-        console.log
-        contenu.innerHTML = html;
-        current_stylesheet.href = "assets/pages/"+nav_elt.id+".css"
-        replaceNav(nav_elt)
-    })
-    .catch(error => {
-        console.error('Erreur:', error);
-    });
+function changerContenu(nav_elt, con = false) {
+    if(con || nav_elt !== current_nav) {
+        console.log("view/"+nav_elt.id+".html")
+        fetch("view/"+nav_elt.id+".html")
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Erreur de chargement du fichier HTML');
+            }
+            return response.text();
+        })
+        .then(html => {
+            contenu.style.opacity = "0"
+            setTimeout(() => {
+                contenu.style.display = "none"
+                contenu.innerHTML = html;
+                current_stylesheet.href = "assets/pages/"+nav_elt.id+".css"
+                let current_script = document.getElementById(current_nav.id+"_script")
+                console.log(current_script)
+                if(current_script !== null) {
+                    document.head.removeChild(current_script)
+                }
+                let script = document.createElement("script")
+                script.id = nav_elt.id+"_script"
+                script.src = '/scripts/pages/'+nav_elt.id+'.js';
+                script.type = 'text/javascript';
+                document.head.appendChild(script)
+                contenu.style.display = "block"
+                contenu.style.opacity = "1"
+            }, 500)
+            replaceNav(nav_elt)
+        })
+        .catch(error => {
+            console.error('Erreur:', error);
+        });
+    }
 }
 
 function replaceNav(nav_elt) {
     current_nav.className = "button-navigation"
     nav_elt.className = "button-navigation-selected"
     current_nav = nav_elt
-    console.log(nav_elt)
 }
 
 const nav = [
@@ -36,7 +54,7 @@ const nav = [
 ]
 
 current_nav = nav[0]
-changerContenu(current_nav)
+changerContenu(current_nav, true)
 
 nav.forEach(elt => {
     elt.addEventListener("click", () => {
--- scripts/pages/accueil.js
+++ scripts/pages/accueil.js
@@ -1,11 +1,3 @@
-const nom_html = document.getElementById("name")
-const nom = "Haron KOCHBATI"
+var nom_html = document.getElementById("name")
+var nom = "Haron KOCHBATI"
 console.log(nom)
-
-for(var i = 0; i < nom.length; i++) {
-    
-    setTimeout(() => {
-        console.log(nom.charAt(i))
-        nom_html.innerHTML += nom.charAt(i)
-    }, 500)
-}
