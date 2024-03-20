03d22dea97c2a6307e3a8123902d3c73cb320e9c
HaronMan <h.kochbati59@gmail.com>
2024-03-20 22:19:29 +0100

    page compétences fait
    PUTAIN DE FOOTER !!!

---
+++ scripts/pages/competences.js
@@ -0,0 +1,44 @@
+var competences = document.getElementById("skills")
+
+function afficherCompetence(skill, delay) {
+    const article = document.createElement("article")
+    article.className = "competence"
+    article.style.animationDelay = 0.5 + 0.2 * delay + "s"
+
+    const details = document.createElement("details")
+    details.innerHTML = "<summary>"+skill.name+"</summary>"+skill.desc
+    details.className = "details"
+
+    const div_barre = document.createElement("div")
+    div_barre.className = "div_barre"
+
+    const barre = document.createElement("div")
+    barre.innerText = skill.value+"%"
+    barre.className = "barre"
+    barre.style.width = skill.value+"%"
+
+    div_barre.appendChild(barre)
+
+    article.appendChild(details)
+    article.appendChild(div_barre)
+    competences.appendChild(article)
+}
+
+var lst = [
+    {"name": "Python", "value": 80, "desc": "Maitrîse de psycopg, matplotlib et random"},
+    {"name": "Java", "value": 90, "desc": "Maîtrise de Spring, Hibernate, Cucumber et JUnit"},
+    {"name": "SQL", "value": 75, "desc": "Maîtrise de PostgreSQL et connaissance en SQLite"},
+    {"name": "C", "value": 25, "desc": "Connaissances de bases"},
+    {"name": "HTML/CSS", "value": 70, "desc": ""},
+    {"name": "PHP", "value": 60, "desc": "Maîtrise de PDO et connaissance en PHPUnit"},
+    {"name": "JavaScript", "value": 65, "desc": "Connaissance en Node.js"},
+    {"name": "Android", "value": 35, "desc": "Utilisation et connaissance de bases"},
+    {"name": "Github", "value": 65, "desc": "Maîtrise assez importante pour mener l'avancée d'un projet à bien"}
+]
+
+var i = 1
+lst.forEach(s => {
+    afficherCompetence(s, i)
+    i++
+});
+
