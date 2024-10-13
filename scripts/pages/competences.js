9bd6c565a7f933c52815ce162e13a06073f51620
HaronMan <h.kochbati59@gmail.com>
2024-10-13 11:16:09 +0200

    MAJ

--- scripts/pages/competences.js
+++ scripts/pages/competences.js
@@ -32,10 +32,10 @@ function afficherCompetence(skill, delay) {
 var lst = [
     {"name": "Python", "value": 80, "desc": "Maitrîse de psycopg, matplotlib et random"},
     {"name": "Java", "value": 90, "desc": "Maîtrise de Spring, Hibernate, Cucumber et JUnit"},
-    {"name": "SQL", "value": 75, "desc": "Maîtrise de PostgreSQL et connaissance en SQLite"},
+    {"name": "SQL", "value": 75, "desc": "Maîtrise de PostgreSQL et connaissance en SQLite et MySQL"},
     {"name": "HTML/CSS", "value": 70, "desc": "Maîtrise assez important pour effectuer une bonne mise en page aisni qu'une bonne structure HTML"},
-    {"name": "PHP", "value": 60, "desc": "Maîtrise de PDO et connaissance en PHPUnit"},
-    {"name": "JavaScript", "value": 65, "desc": "Connaissance en Node.js"},
+    {"name": "PHP", "value": 60, "desc": "Maîtrise de PDO et de Symfony et connaissance en PHPUnit"},
+    {"name": "JavaScript", "value": 75, "desc": "Connaissance en Node.js et en Vue.js"},
     {"name": "Android", "value": 35, "desc": "Utilisation et connaissance de bases"},
     {"name": "Docker", "value": 25, "desc": "Connaissances de bases"},
     {"name": "Github", "value": 65, "desc": "Maîtrise assez importante pour mener l'avancée d'un projet à bien"}
